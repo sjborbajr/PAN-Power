a2ed20e9fc4e86652f23b4f1741a21e782d8473a
Steve Borba <sjborbajr@users.noreply.github.com>
2019-04-05 10:24:17 -0700

    Update Get-PANConnectionsPerSecond.ps1

--- NewTesting/Get-PANConnectionsPerSecond.ps1
+++ NewTesting/Get-PANConnectionsPerSecond.ps1
@@ -148,14 +148,3 @@
   }
   $Statistics
 }
-function Measure-Object-2 {
-  [CmdletBinding()]
-  param (
-    [double[]]$numbers
-  )
-  $Return = $numbers | Measure-Object -Average -Sum -Maximum -Minimum
-  $sqdiffs = $numbers | foreach {[math]::Pow(($psitem - $Return.Average), 2)}
-  $StandardDeviation = [math]::Round([math]::Sqrt( ($sqdiffs | Measure-Object -Average | select -ExpandProperty Average) ), 3)
-  $Return | Add-Member 'StandardDeviation' $StandardDeviation
-  $Return
-}
