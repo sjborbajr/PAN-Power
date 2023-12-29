d5e45ab77d36b278a6d5a47e3f3001a5bcb4fe27
Steve Borba <sjborbajr@users.noreply.github.com>
2023-12-29 12:06:06 -0800

    added target parameter

--- Public/Remove-PANConfig.ps1
+++ Public/Remove-PANConfig.ps1
@@ -9,6 +9,9 @@
 .PARAMETER XPath
     This is location from which to get the config
 
+.PARAMETER Target
+    This parameter allows you to redirect queries through Panorama to a managed firewall
+
 .PARAMETER Addresses
     This is a set of addresses to run the command on, The firewalls must have the same master key for this to work
 
@@ -27,9 +30,10 @@
 
 .NOTES
     Author: Steve Borba https://github.com/sjborbajr/PAN-Power
-    Last Edit: 2019-11-01
+    Last Edit: 2022-12-29
     Version 1.0.7 - added remove config cmdlet
-
+    Version 1.0.8 - added target parameter
+    
 #>
   [CmdletBinding()]
   Param (
@@ -37,6 +41,7 @@
     [Parameter(Mandatory=$False)]  [Switch]    $SkipCertificateCheck,
     [Parameter(Mandatory=$False)]  [string]    $Tag,
     [Parameter(Mandatory=$False)]  [string]    $Path = '',
+    [Parameter(Mandatory=$False)]  [string[]]  $Target,
     [Parameter(Mandatory=$False)]  [string[]]  $Addresses,
     [Parameter(Mandatory=$False)]  [string]    $Key
 )
@@ -73,6 +78,9 @@
   ForEach ($Address in $Addresses) {
     $HashArguments = @{
       URI = "https://"+$Address+"/api/?type=config&action=$Action&XPath=$XPath&"+$Auth
+    }
+    If ($Target) {
+      $HashArguments['URI'] += "&target=$Target"
     }
     If ($SkipCertificateCheck) {
       If ($Host.Version.Major -ge 6) {
